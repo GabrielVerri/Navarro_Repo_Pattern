eaa146e07bfe72ae2504644b52268a5e5673a48e
Gabriel Verri <gabrielvp2004@hotmail.com>
2025-04-06 02:10:45 -0300

    ajustes

---
+++ Navarro_Repo_Pattern.api.web/Controllers/UsuarioController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Navarro_Repo_pattern.Domain;
+using Navarro_Repo_Pattern.Infra.Interface;
+
+namespace Navarro_Repo_Pattern.api.web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> ObterTodos()
+        {
+            var usuarios = await _usuarioRepository.GetAllUsuarios();
+            return Ok(usuarios);
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
